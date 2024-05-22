19388a618f65473a3b45380442b225f87b5b6446
Erik de la Haye <erik@delahaye.de>
2024-05-22 03:09:02 +0200

    Add contravariant kerr metric

--- metrics/metrics.c
+++ metrics/metrics.c
@@ -49,6 +49,18 @@ int metric_schwarzschild_contravariant(tensor4* p_tensor_event, double mass_BH, 
 }
 
 
+int metric_kerr(enum Covariance cov, tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric) {
+    if (cov == COVARIANT) {
+        return metric_kerr_covariant(p_tensor_event, mass_BH, rot_param_a, pp_tensor_metric);
+    } else if (cov == CONTRAVARIANT) {
+        return metric_kerr_contravariant(p_tensor_event, mass_BH, rot_param_a, pp_tensor_metric);
+    } else {
+        fprintf(stderr, "ERROR: Covariance can only be COVARIANT OR CONTRAVARIANT.");
+        return 1;
+    }
+}
+
+
 int metric_kerr_covariant(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric) {
     if (rot_param_a > mass_BH) {
         fprintf(stderr, "WARNING: Rotational parameter a exceeds BH mass. This would result in a naked singularity.");
@@ -69,6 +81,31 @@ int metric_kerr_covariant(tensor4* p_tensor_event, double mass_BH, double rot_pa
     (*pp_tensor_metric)->vals[15] = (radius * radius + rot_param_a * rot_param_a + 2.0 * mass_BH * radius * rot_param_a * rot_param_a / sigma * sin_theta * sin_theta) * sin_theta * sin_theta;
 
     (*pp_tensor_metric)->vals[1] = -2.0 * mass_BH * radius * rot_param_a * sin_theta * sin_theta / sigma;
+    (*pp_tensor_metric)->vals[4] = (*pp_tensor_metric)->vals[1];
+    return 0;
+}
+
+
+int metric_kerr_contravariant(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric) {
+    if (rot_param_a > mass_BH) {
+        fprintf(stderr, "WARNING: Rotational parameter a exceeds BH mass. This would result in a naked singularity.");
+    }
+
+    (*pp_tensor_metric) = tensor4_zeros(2);
+
+    double radius = p_tensor_event->vals[1];
+    double sin_theta = sin(p_tensor_event->vals[2]);
+    double cos_theta = cos(p_tensor_event->vals[2]);
+
+    double sigma = radius * radius + rot_param_a * rot_param_a * cos_theta * cos_theta;
+    double delta = radius * radius - 2.0 * mass_BH * radius + rot_param_a * rot_param_a;
+
+    (*pp_tensor_metric)->vals[0] = -(radius * radius + rot_param_a * rot_param_a + 2.0 * mass_BH * radius * rot_param_a * rot_param_a / sigma * sin_theta * sin_theta) / delta;
+    (*pp_tensor_metric)->vals[5] = delta / sigma;
+    (*pp_tensor_metric)->vals[10] = 1.0 / sigma;
+    (*pp_tensor_metric)->vals[15] = (1 - 2.0 * mass_BH * radius / sigma) / (delta * sin_theta * sin_theta);
+
+    (*pp_tensor_metric)->vals[1] = -2.0 * mass_BH * radius * rot_param_a / (sigma * delta);
     (*pp_tensor_metric)->vals[4] = (*pp_tensor_metric)->vals[1];
     return 0;
 }
--- metrics/metrics.h
+++ metrics/metrics.h
@@ -14,7 +14,11 @@ int metric_schwarzschild_covariant(tensor4* p_tensor_event, double mass_BH, tens
 
 int metric_schwarzschild_contravariant(tensor4* p_tensor_event, double mass_BH, tensor4** pp_tensor_metric);
 
+int metric_kerr(enum Covariance cov, tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric);
+
 int metric_kerr_covariant(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric);
+
+int metric_kerr_contravariant(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric);
 
 // TODO: int metric_wormhole_covariant();
 
