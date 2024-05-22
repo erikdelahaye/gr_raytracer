c741f1143f1d7312c965946a271222af71ef06ed
Erik de la Haye <erik@delahaye.de>
2024-05-22 02:07:42 +0200

    Rename metric functions to clarify covariance

--- metrics/metrics.c
+++ metrics/metrics.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 
-int metric_schwarzschild(tensor4* p_tensor_event, double mass_BH, tensor4** pp_tensor_metric) {
+int metric_schwarzschild_covariant(tensor4* p_tensor_event, double mass_BH, tensor4** pp_tensor_metric) {
     (*pp_tensor_metric) = tensor4_zeros(2);
 
     double radius = p_tensor_event->vals[1];
@@ -18,7 +18,7 @@ int metric_schwarzschild(tensor4* p_tensor_event, double mass_BH, tensor4** pp_t
 }
 
 
-int metric_kerr(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric) {
+int metric_kerr_covariant(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric) {
     if (rot_param_a > mass_BH) {
         fprintf(stderr, "WARNING: Rotational parameter a exceeds BH mass. This would result in a naked singularity.");
     }
--- metrics/metrics.h
+++ metrics/metrics.h
@@ -3,10 +3,10 @@
 
 #include "../tensors/tensor4.h"
 
-int metric_schwarzschild(tensor4* p_tensor_event, double mass_BH, tensor4** pp_tensor_metric);
+int metric_schwarzschild_covariant(tensor4* p_tensor_event, double mass_BH, tensor4** pp_tensor_metric);
 
-int metric_kerr(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric);
+int metric_kerr_covariant(tensor4* p_tensor_event, double mass_BH, double rot_param_a, tensor4** pp_tensor_metric);
 
-// TODO: int metric_wormhole();
+// TODO: int metric_wormhole_covariant();
 
 #endif
