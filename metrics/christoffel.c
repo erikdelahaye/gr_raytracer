14164c7c519d4f44951fba28b7ce8df1792851c4
Erik de la Haye <erik@delahaye.de>
2024-05-31 00:56:13 +0200

    Fix christoffel calculation

--- metrics/christoffel.c
+++ metrics/christoffel.c
@@ -3,9 +3,9 @@
 tensor4* christoffel_symbols(tensor4* p_metric_contravariant, tensor4* p_metric_derivative) {
     // Gamma^sigma_mu-nu = 1/2 g^sigma-rho (g_rho-mu,nu + g_rho-nu,mu - g_mu-nu,rho)
 
-    tensor4* p_sum_2 = tensor4_reorder(p_metric_derivative, "rmn", "rnm");
+    tensor4* p_sum_2 = tensor4_reorder(p_metric_derivative, "rnm", "rmn");
 
-    tensor4* p_sum_3 = tensor4_reorder(p_metric_derivative, "rmn", "mnr");
+    tensor4* p_sum_3 = tensor4_reorder(p_metric_derivative, "mnr", "rmn");
     p_sum_3 = tensor4_scalar_mult(p_sum_3, -1.0);
 
     tensor4* p_sum = tensor4_add(p_metric_derivative, p_sum_2);
@@ -15,5 +15,5 @@ tensor4* christoffel_symbols(tensor4* p_metric_contravariant, tensor4* p_metric_
 
     tensor4* p_tensor_out = tensor4_mult(pp_tensors, "srrmn", 2);
     p_tensor_out = tensor4_scalar_mult(p_tensor_out, 0.5);
-    return 0;
+    return p_tensor_out;
 }
