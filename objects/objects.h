f5ae3f5f4bebe9c011968324ec8a10557a18ce24
Erik de la Haye <erik@delahaye.de>
2024-06-19 20:07:54 +0200

    Add some object drafts

---
+++ objects/objects.h
@@ -0,0 +1,19 @@
+#ifndef OBJECTS_H_IMPORTED
+#define OBJECTS_H_IMPORTED
+
+#include "../tensors/tensor4.h"
+#include "../vectors/vec3.h"
+
+enum Objects {
+    SKYSPHERE,
+    DISK,
+    SPHERE
+};
+
+typedef struct {
+    enum Objects type;
+    vec3 position;
+    double radius;
+} object;
+
+#endif
---
+++ vectors/vec3.h
@@ -0,0 +1,10 @@
+#ifndef VEC3_H_IMPORTED
+#define VEC3_H_IMPORTED
+
+typedef struct {
+    double x1;
+    double x2;
+    double x3;
+} vec3;
+
+#endif
